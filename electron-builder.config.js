2b76d57333b1bdea0cd0b7f5519d5ba06f8e9b0f
brian <brian@toimc.com>
2021-11-19 00:50:20 +0800

    electron-builder配置

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -1,6 +1,8 @@
 if (process.env.VITE_APP_VERSION === undefined) {
-  const now = new Date;
-  process.env.VITE_APP_VERSION = `${now.getUTCFullYear() - 2000}.${now.getUTCMonth() + 1}.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`;
+  const now = new Date()
+  process.env.VITE_APP_VERSION = `${now.getUTCFullYear() - 2000}.${
+    now.getUTCMonth() + 1
+  }.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`
 }
 
 /**
@@ -8,16 +10,38 @@ if (process.env.VITE_APP_VERSION === undefined) {
  * @see https://www.electron.build/configuration/configuration
  */
 const config = {
+  appId: 'com.toimc.community.app',
+  copyright: 'Copyright © 2021 Toimc',
   directories: {
     output: 'dist',
-    buildResources: 'buildResources',
+    buildResources: 'buildResources'
   },
-  files: [
-    'packages/**/dist/**',
-  ],
+  files: ['packages/**/dist/**'],
   extraMetadata: {
-    version: process.env.VITE_APP_VERSION,
+    version: process.env.VITE_APP_VERSION
   },
-};
+  productName: 'toimc技术社区',
+  dmg: {
+    background: 'buildResources/background.tiff'
+  },
+  mac: {
+    category: 'public.app-category.lifestyle',
+    target: 'dmg'
+  },
+  win: {
+    target: ['nsis', 'zip']
+  },
+  nsis: {
+    oneClick: false,
+    allowToChangeInstallationDirectory: true,
+    shortcutName: 'toimc技术社区'
+  },
+  linux: {
+    target: ['deb', 'tar.xz'],
+    category: 'Office',
+    executableName: 'toimc-community',
+    icon: 'buildResources/icon.png'
+  }
+}
 
-module.exports = config;
+module.exports = config
